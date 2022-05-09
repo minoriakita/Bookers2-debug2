3c9a559ec6fb3007243f405f98a2b8f2499ac151
minoriakita <minori.akita@gmail.com>
2022-05-09 15:47:08 +0000

    [Update] 応用課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destoy]
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+
   end
 
   def index
@@ -33,15 +36,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destoy
     @book = Book.find(params[:id])
-    @book.destoy
-    redirect_to books_path
+    @book.destroy
+    redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 end
